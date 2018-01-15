db2e5604131c13d4429f60d7c0f3b4b2dd558692
provenstyle <provenstyle@TeamCity>
2018-01-15 00:20:25 +0000

    TeamCity change in 'DotNet :: NuGet :: kotlin' project: Versioned settings configuration updated

---
+++ .teamcity/NuGet_Kotlin_Improving/Project.kt
@@ -0,0 +1,12 @@
+package NuGet_Kotlin_Improving
+
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+
+object Project : Project({
+    uuid = "2380920a-f24b-410f-b86a-ebb59f87ac22"
+    id = "NuGet_Kotlin_Improving"
+    parentId = "NuGet_Kotlin"
+    name = "Improving"
+    description = "Improving Nugets"
+})
---
+++ .teamcity/NuGet_Kotlin_Miruken/Project.kt
@@ -0,0 +1,12 @@
+package NuGet_Kotlin_Miruken
+
+import jetbrains.buildServer.configs.kotlin.v2017_2.*
+import jetbrains.buildServer.configs.kotlin.v2017_2.Project
+
+object Project : Project({
+    uuid = "eea6b38a-724a-4304-a454-3b6cfb93bfdb"
+    id = "NuGet_Kotlin_Miruken"
+    parentId = "NuGet_Kotlin"
+    name = "Miruken"
+    description = "Miruken-DotNet NuGet Pakages"
+})
