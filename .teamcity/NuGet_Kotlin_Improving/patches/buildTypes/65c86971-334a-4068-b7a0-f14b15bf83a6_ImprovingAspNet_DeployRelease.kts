ae62c4473e4770dbf2f302900e1ecff4020e9f59
provenstyle <provenstyle@TeamCity>
2018-01-19 23:11:42 +0000

    TeamCity change in 'DotNet :: NuGet :: Configured with Kotlin DSL :: Improving :: Improving.AspNet Solution :: Deployment :: Improving.AspNet' project: runners of 'Deploy Release' build configuration were updated

--- .teamcity/NuGet_Kotlin_Improving/patches/buildTypes/65c86971-334a-4068-b7a0-f14b15bf83a6_ImprovingAspNet_DeployRelease.kts
+++ .teamcity/NuGet_Kotlin_Improving/patches/buildTypes/65c86971-334a-4068-b7a0-f14b15bf83a6_ImprovingAspNet_DeployRelease.kts
@@ -39,7 +39,7 @@ changeBuildType("65c86971-334a-4068-b7a0-f14b15bf83a6_ImprovingAspNet_DeployRele
         }
         insert(2) {
             step {
-                name = "Nuget Publish to NuGet.org (1)"
+                name = "Nuget Publish to SymbolSource.org"
                 type = "jb.nuget.publish"
                 param("secure:nuget.api.key", "credentialsJSON:c17afa04-30a4-410b-ac55-f2d0fc02578d")
                 param("nuget.path", "%teamcity.tool.NuGet.CommandLine.DEFAULT%")
