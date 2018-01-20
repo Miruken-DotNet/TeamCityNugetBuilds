e16cb9d1ade21dea67771c15db86e87059544833
provenstyle <provenstyle@TeamCity>
2018-01-20 05:44:08 +0000

    no comments

--- .teamcity/NuGet_Kotlin_Improving/patches/projects/65c86971-334a-4068-b7a0-f14b15bf83a6.kts
+++ .teamcity/NuGet_Kotlin_Improving/patches/projects/65c86971-334a-4068-b7a0-f14b15bf83a6.kts
@@ -15,7 +15,7 @@ changeProject("65c86971-334a-4068-b7a0-f14b15bf83a6") {
             param("PatchVersion", "3")
         }
         update {
-            param("PatchVersion", "12")
+            param("PatchVersion", "13")
         }
     }
 }
