4d86057a1508cf2f41926cc02db9f39a509eef09
michael dudley <michael.dudley@improvingenterprises.com>
2019-10-30 16:58:33 -0500

    setting up the core build and test steps

--- .teamcity/miruken/nuget/buildTemplates/CoreFramework.kt
+++ .teamcity/miruken/nuget/buildTemplates/CoreFramework.kt
@@ -1,29 +1,19 @@
 package miruken.nuget.buildTemplates
 
-import jetbrains.buildServer.configs.kotlin.v2018_2.BuildStep
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.VisualStudioStep
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.visualStudio
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.vstest
 import jetbrains.buildServer.configs.kotlin.v2018_2.Project
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.*
 
 class CoreFramework {
     companion object {
 
         private fun compile(buildType: BuildType) : BuildType{
             buildType.steps {
-                visualStudio {
-                    name                = "CompileStep"
-                    id                  = "${buildType.id}_Build"
-                    path                = "%Solution%"
-                    version             = VisualStudioStep.VisualStudioVersion.vs2017
-                    runPlatform         = VisualStudioStep.Platform.x86
-                    msBuildVersion      = VisualStudioStep.MSBuildVersion.V15_0
-                    msBuildToolsVersion = VisualStudioStep.MSBuildToolsVersion.V15_0
-                    targets             = "%BuildTargets%"
-                    configuration       = "%BuildConfiguration%"
-                    platform            = "Any CPU"
-                    executionMode       = BuildStep.ExecutionMode.RUN_ON_SUCCESS
+                dotnetBuild {
+                    name = "Compile"
+                    projects = "Miruken.sln"
+                    configuration = "Debug"
+                    param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
                 }
             }
             return buildType
@@ -31,16 +21,10 @@ class CoreFramework {
 
         private fun test(buildType: BuildType) : BuildType{
             buildType.steps {
-                vstest {
-                    id                   = "${buildType.id}_TestStep"
-                    vstestPath           = "%teamcity.dotnet.vstest.14.0%"
-                    includeTestFileNames = "%TestAssemblies%"
-                    runSettings          = ""
-                    testCaseFilter       = "%TestCaseFilter%"
-                    executionMode        = BuildStep.ExecutionMode.RUN_ON_SUCCESS
-                    coverage = dotcover {
-                        toolPath = "%teamcity.tool.JetBrains.dotCover.CommandLineTools.bundled%"
-                    }
+                dotnetTest {
+                    name = "Unit Tests"
+                    projects = "Test/*/*.csproj"
+                    param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
                 }
             }
             return buildType
