d482b42bfc696a8676f673f6f55c6b8d24fc7730
provenstyle <provenstyle@TeamCity>
2021-02-15 00:42:12 +0000

    no comments

--- .teamcity/NuGet_Kotlin_MirukenCore/patches/projects/b4054a6e-e8bc-4f1e-868d-c01d8c1d669b.kts
+++ .teamcity/NuGet_Kotlin_MirukenCore/patches/projects/b4054a6e-e8bc-4f1e-868d-c01d8c1d669b.kts
@@ -21,7 +21,7 @@ changeProject(uuid("b4054a6e-e8bc-4f1e-868d-c01d8c1d669b")) {
             param("PatchVersion", "1")
         }
         update {
-            param("PatchVersion", "6")
+            param("PatchVersion", "7")
         }
     }
 }
